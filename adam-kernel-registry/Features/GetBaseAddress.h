622daa5dd3538ca3eba88f1c0e746190ef841772
Adam <103837300+amdadm@users.noreply.github.com>
2025-04-29 13:32:22 +0200

    Update GetBaseAddress.h

--- adam-kernel-registry/Features/GetBaseAddress.h
+++ adam-kernel-registry/Features/GetBaseAddress.h
@@ -6,8 +6,8 @@ uintptr_t GetBaseAddress(int ProcessPid) {
 
     PEPROCESS TargetProcess = nullptr;
     
-    NTSTATUS  status = PsLookupProcessByProcessId(HANDLE(ProcessPid), &TargetProcess);
-    if (!NT_SUCCESS(status))
+    NTSTATUS  Status = PsLookupProcessByProcessId(HANDLE(ProcessPid), &TargetProcess);
+    if (!NT_SUCCESS(Status))
         return 0;
 
     const auto BaseAddress = uintptr_t(PsGetProcessSectionBaseAddress(TargetProcess));
